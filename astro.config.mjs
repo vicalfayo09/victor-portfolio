899adced17ddf7105304b15b591b6a57c8196bbf
victoralfayo <victor.opoti@strathmore.edu>
2023-04-06 17:28:52 +0300

    changed gitignore

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    base: '/dist',
+    site: 'https://alfayo-victor-portfolio.netlify.app',
+    output: 'static',
+});
