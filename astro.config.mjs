808577a2d27e74014a145a8779185ac9a6ecd99b
victoralfayo <victor.opoti@strathmore.edu>
2023-04-06 22:46:23 +0300

    site change link

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     base: '/dist',
-    site: 'https://alfayo-victor-portfolio.netlify.app',
+    site: 'https://victor-portfolio-dev-site.netlify.app',
     output: 'static',
 });
